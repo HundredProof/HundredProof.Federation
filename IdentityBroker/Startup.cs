a430e059eef1f5941d1682ddf0066e72335970f7
Dave Campbell <ghostofrecon@gmail.com>
2020-02-15 12:26:50 -0600

    removed comments

--- IdentityBroker/Startup.cs
+++ IdentityBroker/Startup.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using HundredProof.Federation.Domain.Account;
+﻿using HundredProof.Federation.Domain.Account;
 using HundredProof.Federation.Domain.EF;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
