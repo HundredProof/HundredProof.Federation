eb73a1fa2d2b6520d917da8302740bd538d5e637
Dave Campbell <ghostofrecon@yahoo.com>
2020-02-23 16:06:57 -0600

    Pulled out Broker Login POST method.  Not needed in Broker.

--- IdentityBroker/Controllers/Account/AccountController.cs
+++ IdentityBroker/Controllers/Account/AccountController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using HundredProof.Federation.DataModel.UserDatabase;
 using HundredProof.Federation.Domain;
@@ -8,7 +7,6 @@ using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Events;
 using IdentityServer4.Extensions;
-using IdentityServer4.Models;
 using IdentityServer4.Services;
 using IdentityServer4.Stores;
 using Microsoft.AspNetCore.Authentication;
@@ -47,94 +45,21 @@ namespace IdentityBroker.Controllers.Account {
         /// </summary>
         [HttpGet]
         public async Task<IActionResult> Login(string returnUrl) {
-            // build a model so we know what to show on the login page
             var vm = await BuildLoginViewModelAsync(returnUrl);
 
             if (vm.IsExternalLoginOnly)
-                // we only have one option for logging in and it's an external provider
                 return RedirectToAction("Challenge", "External", new {provider = vm.ExternalLoginScheme, returnUrl});
 
             return View(vm);
         }
 
         /// <summary>
-        ///     Handle postback from username/password login
-        /// </summary>
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginInputModel model, string button) {
-            // check if we are in the context of an authorization request
-            var context = await _interaction.GetAuthorizationContextAsync(model.ReturnUrl);
-
-            // the user clicked the "cancel" button
-            if (button != "login") {
-                if (context != null) {
-                    // if the user cancels, send a result back into IdentityServer as if they 
-                    // denied the consent (even if this client does not require consent).
-                    // this will send back an access denied OIDC error response to the client.
-                    await _interaction.GrantConsentAsync(context, ConsentResponse.Denied);
-
-                    // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-                    if (await _clientStore.IsPkceClientAsync(context.ClientId))
-                        // if the client is PKCE then we assume it's native, so this change in how to
-                        // return the response is for better UX for the end user.
-                        return View("Redirect", new RedirectViewModel {RedirectUrl = model.ReturnUrl});
-
-                    return Redirect(model.ReturnUrl);
-                }
-
-                // since we don't have a valid context, then we just go back to the home page
-                return Redirect("~/");
-            }
-
-            if (ModelState.IsValid) {
-                var result =
-                    await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberLogin, true);
-                if (result.Succeeded) {
-                    var user = await _userManager.FindByNameAsync(model.Username);
-                    await _events.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.UserName,
-                        clientId: context?.ClientId));
-
-                    if (context != null) {
-                        if (await _clientStore.IsPkceClientAsync(context.ClientId))
-                            // if the client is PKCE then we assume it's native, so this change in how to
-                            // return the response is for better UX for the end user.
-                            return View("Redirect", new RedirectViewModel {RedirectUrl = model.ReturnUrl});
-
-                        // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-                        return Redirect(model.ReturnUrl);
-                    }
-
-                    // request for a local page
-                    if (Url.IsLocalUrl(model.ReturnUrl))
-                        return Redirect(model.ReturnUrl);
-                    if (string.IsNullOrEmpty(model.ReturnUrl))
-                        return Redirect("~/");
-                    throw new Exception("invalid return URL");
-                }
-
-                await _events.RaiseAsync(new UserLoginFailureEvent(model.Username, "invalid credentials",
-                    clientId: context?.ClientId));
-                ModelState.AddModelError(string.Empty, AccountOptions.InvalidCredentialsErrorMessage);
-            }
-
-            // something went wrong, show form with error
-            var vm = await BuildLoginViewModelAsync(model);
-            return View(vm);
-        }
-
-
-        /// <summary>
         ///     Show logout page
         /// </summary>
         [HttpGet]
         public async Task<IActionResult> Logout(string logoutId) {
-            // build a model so the logout page knows what to display
             var vm = await BuildLogoutViewModelAsync(logoutId);
-
             if (vm.ShowLogoutPrompt == false)
-                // if the request for logout was properly authenticated from IdentityServer, then
-                // we don't need to show the prompt and can just log the user out directly.
                 return await Logout(vm);
 
             return View(vm);
@@ -145,26 +70,14 @@ namespace IdentityBroker.Controllers.Account {
         /// </summary>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout(LogoutInputModel model) {
-            // build a model so the logged out page knows what to display
+        public async Task<IActionResult> Logout(LogoutInputModel model) { 
             var vm = await BuildLoggedOutViewModelAsync(model.LogoutId);
-
             if (User?.Identity.IsAuthenticated == true) {
-                // delete local authentication cookie
                 await _signInManager.SignOutAsync();
-
-                // raise the logout event
                 await _events.RaiseAsync(new UserLogoutSuccessEvent(User.GetSubjectId(), User.GetDisplayName()));
             }
-
-            // check if we need to trigger sign-out at an upstream identity provider
             if (vm.TriggerExternalSignout) {
-                // build a return URL so the upstream provider will redirect back
-                // to us after the user has logged out. this allows us to then
-                // complete our single sign-out processing.
                 var url = Url.Action("Logout", new {logoutId = vm.LogoutId});
-
-                // this triggers a redirect to the external provider for sign-out
                 return SignOut(new AuthenticationProperties {RedirectUri = url}, vm.ExternalAuthenticationScheme);
             }
 
@@ -184,8 +97,6 @@ namespace IdentityBroker.Controllers.Account {
             var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
             if (context?.IdP != null && await _schemeProvider.GetSchemeAsync(context.IdP) != null) {
                 var local = context.IdP == IdentityServerConstants.LocalIdentityProvider;
-
-                // this is meant to short circuit the UI and only trigger the one external IdP
                 var vm = new LoginViewModel {
                     EnableLocalLogin = local,
                     ReturnUrl = returnUrl,
@@ -236,26 +147,19 @@ namespace IdentityBroker.Controllers.Account {
         private async Task<LogoutViewModel> BuildLogoutViewModelAsync(string logoutId) {
             var vm = new LogoutViewModel {LogoutId = logoutId, ShowLogoutPrompt = AccountOptions.ShowLogoutPrompt};
 
-            if (User?.Identity.IsAuthenticated != true) {
-                // if the user is not authenticated, then just show logged out page
+            if (User?.Identity.IsAuthenticated != true) { 
                 vm.ShowLogoutPrompt = false;
                 return vm;
             }
-
             var context = await _interaction.GetLogoutContextAsync(logoutId);
             if (context?.ShowSignoutPrompt == false) {
-                // it's safe to automatically sign-out
                 vm.ShowLogoutPrompt = false;
                 return vm;
             }
-
-            // show the logout prompt. this prevents attacks where the user
-            // is automatically signed out by another malicious web page.
             return vm;
         }
 
         private async Task<LoggedOutViewModel> BuildLoggedOutViewModelAsync(string logoutId) {
-            // get context information (client name, post logout redirect URI and iframe for federated signout)
             var logout = await _interaction.GetLogoutContextAsync(logoutId);
 
             var vm = new LoggedOutViewModel {
@@ -272,16 +176,11 @@ namespace IdentityBroker.Controllers.Account {
                     var providerSupportsSignout = await HttpContext.GetSchemeSupportsSignOutAsync(idp);
                     if (providerSupportsSignout) {
                         if (vm.LogoutId == null)
-                            // if there's no current logout context, we need to create one
-                            // this captures necessary info from the current logged in user
-                            // before we signout and redirect away to the external IdP for signout
                             vm.LogoutId = await _interaction.CreateLogoutContextAsync();
-
                         vm.ExternalAuthenticationScheme = idp;
                     }
                 }
             }
-
             return vm;
         }
     }
--- IdentityEndpoint/Controllers/Account/ExternalController.cs
+++ IdentityEndpoint/Controllers/Account/ExternalController.cs
@@ -72,7 +72,6 @@ namespace IdentityEndpoint.Controllers.Account {
         /// </summary>
         [HttpGet]
         public async Task<IActionResult> Callback() {
-            // read external identity from the temporary cookie
             var result = await HttpContext.AuthenticateAsync(IdentityConstants.ExternalScheme);
             if (result?.Succeeded != true) throw new Exception("External authentication error");
 
@@ -80,47 +79,25 @@ namespace IdentityEndpoint.Controllers.Account {
                 var externalClaims = result.Principal.Claims.Select(c => $"{c.Type}: {c.Value}");
                 _logger.LogDebug("External claims: {@claims}", externalClaims);
             }
-
-            // lookup our user and external provider info
             var (user, provider, providerUserId, claims) = await FindUserFromExternalProviderAsync(result);
             if (user == null)
-                // this might be where you might initiate a custom workflow for user registration
-                // in this sample we don't show how that would be done, as our sample implementation
-                // simply auto-provisions new external user
                 user = await AutoProvisionUserAsync(provider, providerUserId, claims);
-
-            // this allows us to collect any additonal claims or properties
-            // for the specific prtotocols used and store them in the local auth cookie.
-            // this is typically used to store data needed for signout from those protocols.
             var additionalLocalClaims = new List<Claim>();
             var localSignInProps = new AuthenticationProperties();
             ProcessLoginCallbackForOidc(result, additionalLocalClaims, localSignInProps);
             ProcessLoginCallbackForWsFed(result, additionalLocalClaims, localSignInProps);
             ProcessLoginCallbackForSaml2p(result, additionalLocalClaims, localSignInProps);
-
-            // issue authentication cookie for user
-            // we must issue the cookie maually, and can't use the SignInManager because
-            // it doesn't expose an API to issue additional claims from the login workflow
             var principal = await _signInManager.CreateUserPrincipalAsync(user);
             additionalLocalClaims.AddRange(principal.Claims);
             var name = principal.FindFirst(JwtClaimTypes.Name)?.Value ?? user.Id;
             await HttpContext.SignInAsync(user.Id, name, provider, localSignInProps, additionalLocalClaims.ToArray());
-
-            // delete temporary cookie used during external authentication
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-
-            // retrieve return URL
             var returnUrl = result.Properties.Items["returnUrl"] ?? "~/";
-
-            // check if external login is in the context of an OIDC request
             var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
             await _events.RaiseAsync(new UserLoginSuccessEvent(provider, providerUserId, user.Id, name, true,
                 context?.ClientId));
-
             if (context != null)
                 if (await _clientStore.IsPkceClientAsync(context.ClientId))
-                    // if the client is PKCE then we assume it's native, so this change in how to
-                    // return the response is for better UX for the end user.
                     return View("Redirect", new RedirectViewModel {RedirectUrl = returnUrl});
 
             return Redirect(returnUrl);
@@ -129,33 +106,20 @@ namespace IdentityEndpoint.Controllers.Account {
         private async Task<(ApplicationUser user, string provider, string providerUserId, IEnumerable<Claim> claims)>
             FindUserFromExternalProviderAsync(AuthenticateResult result) {
             var externalUser = result.Principal;
-
-            // try to determine the unique id of the external user (issued by the provider)
-            // the most common claim type for that are the sub claim and the NameIdentifier
-            // depending on the external provider, some other claim type might be used
             var userIdClaim = externalUser.FindFirst(JwtClaimTypes.Subject) ??
                               externalUser.FindFirst(ClaimTypes.NameIdentifier) ??
                               throw new Exception("Unknown userid");
-
-            // remove the user id claim so we don't include it as an extra claim if/when we provision the user
             var claims = externalUser.Claims.ToList();
             claims.Remove(userIdClaim);
-
             var provider = result.Properties.Items["scheme"];
             var providerUserId = userIdClaim.Value;
-
-            // find external user
             var user = await _userManager.FindByLoginAsync(provider, providerUserId);
-
             return (user, provider, providerUserId, claims);
         }
 
         private async Task<ApplicationUser> AutoProvisionUserAsync(string provider, string providerUserId,
             IEnumerable<Claim> claims) {
-            // create a list of claims that we want to transfer into our store
             var filtered = new List<Claim>();
-
-            // user's display name
             var name = claims.FirstOrDefault(x => x.Type == JwtClaimTypes.Name)?.Value ??
                        claims.FirstOrDefault(x => x.Type == ClaimTypes.Name)?.Value;
             if (name != null) {
